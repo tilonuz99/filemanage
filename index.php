e9a41f4cef433bb387b2944597d036f22007d6d3
tilonuz99 <59096186+tilonuz99@users.noreply.github.com>
2020-04-06 20:54:58 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "salom";
-?>
+
+//Get a list of file paths using the glob function.
+$fileList = glob('test/*');
+
+//Loop through the array that glob returned.
+foreach($fileList as $filename){
+   //Simply print them out onto the screen.
+   echo $filename, '<br>'; 
+}
